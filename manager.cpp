8d566156cc328076c4ba7be77dcc225ba938834a
Drew Ripberger <drew.ripberger@gmail.com>
2018-02-03 04:03:06 -0500

    fixed connection issue and mpi testing

--- manager.cpp
+++ manager.cpp
@@ -1,22 +1,45 @@
+#include <mpi.h>
 #include <bitcoinapi/bitcoinapi.h>
 #include <string>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
 
-const std::string username = "rdsf";
+const int start = 10; // 10 Seconds
+const int end = 60 * 10; // 10 Minutes
+
+const std::string username = "user";
 const std::string password = "password";
-const std::string addr = "127.0.0.1"; // Localhost
-const int port = 18444; // Default regtest port 
+const std::string addr = "localhost"; // Localhost
+const int port = 2222; // Default regtest port 
 const std::string recp = "miNcNbbqp39Th3QYkiqTXW2QauQ5JKcmjR"; // Throwaway address 
 const double amt = 0.0001; // Small amount of bitcoin for rapid testing
-	
-int main(){
 
-	try{
-		BitcoinAPI client(username, password, addr, port);
-		//client.sendcommmand("generate", 101);
-	
-		client.sendtoaddress(recp, amt);
-	}
-	catch(BitcoinException e){
-		std::cerr << e.getMessage() << std::endl;
-	}
+int main(){
+	// Initialization Code for MPI specific utilities
+	MPI_Init(NULL, NULL);
+
+	// Stores the total number of processes in the network
+	int worldSize;
+	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+	// Stores the rank(identification # starting at 0) of the current process
+	int worldRank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+
+	// Connecting with running bitcoin daemon
+	BitcoinAPI client(username, password, addr, port);
+	std::cout << "Node" << worldRank << " connecting to the daemon @ " << addr << ":" << port << "..." << std::endl;
+	sleep(1);
+
+	// Generates New Blocks (Unlocks coinbase)
+	Value params;
+	params.append(101);
+	client.sendcommand("generate", params);
+
+
+
+	MPI_Finalize();
 }
