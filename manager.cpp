eca63c556f923b5ac4dd53c851c2d9d8b79a5321
Drew Ripberger <drew.ripberger@gmail.com>
2018-02-05 19:52:14 -0500

    testing addr

--- manager.cpp
+++ manager.cpp
@@ -19,7 +19,7 @@ const std::string username = "user";
 const std::string password = "password";
 const std::string addr = "localhost"; // localhost
 const int port = 2222; // Arbitrary port for specific testing
-const std::string recp = "moRcYXywEzDXZAmnZypuv7SnjgLXGNKTep"; // Throwaway address 
+const std::string recp = "3FkenCiXpSLqD8L79intRNXUgjRoH9sjXa"; // Throwaway address 
 const double amt = 0.0001; // Small amount of bitcoin for rapid testing
 
 const std::string hosts[6] = {"master", "node0", "node1", "node2", "node3", "node4"};
