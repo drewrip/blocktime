31fed8f802b09bc88881ae7cb7cd2c8687a342d7
THE-COB <jmlife6@gmail.com>
2018-02-08 17:46:11 -0500

    Added datafiles dir for updating csv to google drive

---
+++ datafiles/update.py
@@ -0,0 +1,30 @@
+from apiclient import discovery, http
+from httplib2 import Http
+from oauth2client import file, client, tools
+import sys
+
+SCOPES = (
+	'https://www.googleapis.com/auth/drive',
+	'https://www.googleapis.com/auth/drive.appdata',
+	'https://www.googleapis.com/auth/drive.file',
+	'https://www.googleapis.com/auth/drive.readonly.metadata'
+)
+
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+drive = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+
+files = drive.files().list().execute().get('files', [])
+for i in files:
+	if(i["name"] == "BlockTimeFiles"):
+		dirId = i["id"]
+		break
+
+file_metadata = {'name': 'blocktime'+sys.argv[1]+'.csv', "parents":[dirId]}
+media = http.MediaFileUpload('timedata.csv',mimetype='text/csv')
+file = drive.files().create(body=file_metadata,media_body=media,fields='id')
+
+file.execute()
